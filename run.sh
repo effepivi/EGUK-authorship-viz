17a1d577e98bf8575c96621590bc91970cc70d70
effepivi <franck.p.vidal@gmail.com>
2020-06-01 11:47:24 +0100

    Rename the script.

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ CSV_OUTPUT=data/eguk_database.csv
 DB_OUTPUT=data/eguk_database.db
 
 rm -f $CSV_OUTPUT
-src/python/BibViz.py $BIB_FILES $CSV_OUTPUT
+src/python/bib2csv.py $BIB_FILES $CSV_OUTPUT
 
-rm -f $DB_OUTPUT
 src/python/csv2SQLlight.py $CSV_OUTPUT $DB_OUTPUT
 sqlite3 $DB_OUTPUT
+
