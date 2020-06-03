70a2df61d5807761bd26815f36863eb7f51c8099
effepivi <franck.p.vidal@gmail.com>
2020-06-03 14:13:15 +0100

    Do not extract the nodes and edges.

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 BIB_FILES=`ls data/*.bib`
 CSV_OUTPUT=data/eguk_database.csv
 DB_OUTPUT=data/eguk_database.db
-NODES_CSV=data/nodes.csv
-EDGES_CSV=data/edges.csv
 GRAPH_JSON=eguk_authorship_network.json
 
 rm -f $CSV_OUTPUT
@@ -17,6 +15,4 @@ src/python/csv2SQLlight.py $CSV_OUTPUT $DB_OUTPUT
 # sqlite3 $DB_OUTPUT
 
 rm -f $NODES_CSV $EDGES_CSV
-src/python/db2nodes.py $DB_OUTPUT > $NODES_CSV
-src/python/db2edges.py $DB_OUTPUT > $EDGES_CSV
 src/python/db2json.py $DB_OUTPUT > $GRAPH_JSON
